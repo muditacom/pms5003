5bc328211923290de45fe03423ba5355c14c0528
pholat <pholat@gmail.com>
2019-08-05 22:51:42 +0200

    Added kill handler, log to file and timestamp

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 pms5003: pms5003.c
 	indent -kr -i2 -nut $< || true
-	gcc -O3 -g -Werror -Wall -Wextra -pedantic -o $@ $<
+	gcc -std=gnu11 -O3 -g -Werror -Wall -Wextra -pedantic -o $@ $<
 
 all: pms5003
--- pms5003.c
+++ pms5003.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -8,8 +9,10 @@
 #include <byteswap.h>
 #include <signal.h>
 #include <time.h>
+#include <stdarg.h>
 
 #define T_VERSION 0
+FILE *g_log, *g_fd;
 
 #if T_VERSION == 0 // non-T sensor version
   #define DATA11_STRING "count_5um"
@@ -72,6 +75,28 @@ void forced_read(int fd, void *buf, size_t count)
 
 time_t start_time;
 
+void print_wrap(char* fmt, ...)
+{
+    char* str=NULL;
+   va_list args;
+   va_start(args, fmt);
+   int ret=vasprintf(&str,fmt, args);
+   va_end(args);
+
+   if(g_log != NULL) {
+       fprintf(g_log,"%s\n", str);
+   }
+
+   if(g_fd != NULL) {
+       fprintf(g_fd,"%s\n", str);
+   }
+   if(ret<=0) {
+       fprintf(stderr,"vasprintf error! %s\n", strerror(errno));
+   }
+   fprintf(stdout,"%s\n", str);
+   free(str);
+}
+
 void print_data(size_t num_measurements, double *data)
 {
   static const char *labels[] = { "std_pm1", "std_pm2_5", "std_pm10",
@@ -84,23 +109,38 @@ void print_data(size_t num_measurements, double *data)
   time(&current_time);
   double diff_time = difftime(current_time, start_time);
 
-  printf("{\"timestamp\":%.1f,\"num_measurements\":%lu", diff_time, num_measurements);
+  print_wrap("{\"t: %lu, dt\":%.1f,\"num_measurements\":%lu", current_time, diff_time, num_measurements);
 
 #if T_VERSION == 1
   data[10] = data[10] / 10.0;
   data[11] = data[11] / 10.0;
 #endif
 
-  for (size_t i = 3; i < 12; i++)
-    printf(",\"%s\":%.02f", labels[i], data[i]);
+  for (size_t i = 3; i < 12; i++) {
+    print_wrap(",\"%s\":%.02f", labels[i], data[i]);
+  }
 
-  printf("}\n");
+  print_wrap("}\n");
 }
 
 double avg_sum[12] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 size_t avg_num = 0;
 
+void kill_handler(int i)
+{
+    printf("Kill %d!\n", i);
+    if(g_log != 0) {
+        fflush(g_log);
+        fclose(g_log);
+        printf("Close log\n");
+    }
+    if(g_fd != 0) {
+        fflush(g_fd);
+        fclose(g_fd);
+        printf("Close fd\n");
+    }
+}
 void sigalrm_handler()
 {
   if (avg_num > 0) {
@@ -115,69 +155,78 @@ void sigalrm_handler()
   }
 }
 
-int main(int argc, char **argv)
-{
-  if (argc != 2 && argc != 3) {
-    fprintf(stderr,
-            "usage: %s <port> [ <average-over-this-many-seconds>]\n",
-            argv[0]);
-    exit(-1);
-  }
-
-  size_t average_over = 0;
-  if (argc == 3) {
-    average_over = atoi(argv[2]);
-    signal(SIGALRM, sigalrm_handler);
-    alarm(average_over);
-  }
-
-  const char *portname = argv[1];
-  int fd;
-
-  fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-  if (fd < 0) {
-    fprintf(stderr, "fatal: open(): %s: %s\n", portname, strerror(errno));
-    exit(-1);
-  }
-
-  set_interface_attribs(fd, B9600);
-  time(&start_time);
-  for (;;) {
-    char ch;
-    forced_read(fd, &ch, 1);
-    if (ch != 0x42)
-      continue;
-    forced_read(fd, &ch, 1);
-    if (ch != 0x4d)
-      continue;
-
-    unsigned short data[15];
-    forced_read(fd, &data, sizeof(data));
-    for (size_t i = 0; i < sizeof(data) / sizeof(short); i++)
-      data[i] = __bswap_16(data[i]);
-
-    // Check frame length.
-    if (data[0] != 2 * 13 + 2)
-      continue;
-
-    // Check sum.
-    size_t check_sum = 0x42 + 0x4d;
-    for (size_t i = 0; i < sizeof(data) - sizeof(data[0]); i++)
-      check_sum += ((unsigned char *) data)[i];
-    if (check_sum != data[14])
-      continue;
-
-    double data_fp[12];
-    for (size_t i = 0; i < sizeof(avg_sum) / sizeof(avg_sum[0]); i++) {
-      data_fp[i] = data[i + 1];
-      avg_sum[i] += data_fp[i];
+int main(int argc, char **argv) {
+    if (argc != 2 && argc != 3) {
+        fprintf(stderr, "usage: %s <port> [ <average-over-this-many-seconds>]\n", argv[0]);
+        exit(-1);
     }
-    avg_num++;
 
-    // Print each frame if not averaging over time.
-    if (!average_over)
-      print_data(1, data_fp);
-  }
+    signal(SIGKILL, kill_handler);
+    signal(SIGINT, kill_handler);
 
-  close(fd);                    // Won't happen...
+    size_t average_over = 0;
+    if (argc == 3) {
+        average_over = atoi(argv[2]);
+        signal(SIGALRM, sigalrm_handler);
+        alarm(average_over);
+    }
+
+    const char *portname = argv[1];
+    int fd = 0;
+
+    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0) {
+        fprintf(stderr, "fatal: open(): %s: %s\n", portname, strerror(errno));
+        exit(-1);
+    }
+    g_log = fopen("./log.log", "w+");
+    if (g_log == NULL) {
+        printf("Error: ./log.log %s\n", strerror(errno));
+    }
+    g_fd = fdopen(fd, "rw+");
+    if (g_fd == NULL) {
+        printf("Error: %s %s\n", portname, strerror(errno));
+    }
+    print_wrap("LOG START\n");
+
+    set_interface_attribs(fd, B9600);
+    time(&start_time);
+    for (;;) {
+        char ch;
+        forced_read(fd, &ch, 1);
+        if (ch != 0x42)
+            continue;
+        forced_read(fd, &ch, 1);
+        if (ch != 0x4d)
+            continue;
+
+        unsigned short data[15];
+        forced_read(fd, &data, sizeof(data));
+        for (size_t i = 0; i < sizeof(data) / sizeof(short); i++)
+            data[i] = __bswap_16(data[i]);
+
+        // Check frame length.
+        if (data[0] != 2 * 13 + 2)
+            continue;
+
+        // Check sum.
+        size_t check_sum = 0x42 + 0x4d;
+        for (size_t i = 0; i < sizeof(data) - sizeof(data[0]); i++)
+            check_sum += ((unsigned char *)data)[i];
+        if (check_sum != data[14])
+            continue;
+
+        double data_fp[12];
+        for (size_t i = 0; i < sizeof(avg_sum) / sizeof(avg_sum[0]); i++) {
+            data_fp[i] = data[i + 1];
+            avg_sum[i] += data_fp[i];
+        }
+        avg_num++;
+
+        // Print each frame if not averaging over time.
+        if (!average_over)
+            print_data(1, data_fp);
+    }
+
+    close(fd); // Won't happen...
 }
